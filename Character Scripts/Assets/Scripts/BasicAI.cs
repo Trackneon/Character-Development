0dc4cb6c8f2ccb516257837808dc01f7fb0e5d48
Tyler Hales <10869186@my.uvu.edu>
2020-02-09 23:13:49 -0700

    2/9 more

---
+++ Character Scripts/Assets/Scripts/BasicAI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BasicAI : MonoBehaviour {
+
+
+    private NavMeshAgent agent;
+    public Transform Destination;
+    public FloatData data;
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Destination != null) agent.destination = Destination.position;
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        var hit = collision.gameObject;
+        var health = hit.GetComponent<HealthScript>();
+        if (health != null)
+        {
+            health.LoseHealth(data);
+        }
+    }
+}
---
+++ Character Scripts/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EnemyAI : MonoBehaviour {
+
+
+    public float speed;
+    private Transform target;
+    public bool isFollowing;
+    public enum State
+    {idle, isFollowing }
+    public State state;
+    public GameObject player;
+    public FloatData data;
+
+
+	// Use this for initialization
+	void Start () {
+        target = player.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        switch (state) {
+            case State.idle:
+                break;
+            case State.isFollowing:
+                transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+                break;
+        }
+
+
+	}
+
+    void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            //isFollowing = true;
+            state = State.isFollowing;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            state = State.idle;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        var hit = collision.gameObject;
+        var health = hit.GetComponent<HealthScript>();
+        if (health  != null)
+        {
+            health.LoseHealth(data);
+        }
+    }
+}
