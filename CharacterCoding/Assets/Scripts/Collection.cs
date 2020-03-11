87c5043253ec7f80e967007857d6b8024b107915
Tyler Hales <10869186@my.uvu.edu>
2020-03-10 23:04:37 -0600

    3/10
    Collections

---
+++ CharacterCoding/Assets/Scripts/Collection.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Collection : ScriptableObject
+{
+    public List<Collectable> collectableList;
+    public int currentCollectableNum;
+
+    public void AddToCollection(Collectable collectableObj)
+    {
+        if (collectableList.Contains(collectableObj))
+        {
+            return;
+            collectableList.Add(collectableObj);
+        }  
+                
+    }
+
+    public void RemoveFromCollection(Collectable collectableObj)
+    {
+        for (int i = 0; i < collectableList.Count; i--)
+        {
+            Collectable obj = collectableList[i];
+            if (obj == collectableObj)
+            {
+                collectableList.Remove(collectableObj);
+            }
+        }
+    }
+
+    public void ClearCollection()
+    {
+        collectableList.Clear();
+    }
+
+    public void UseCurrentItem()
+    {
+        if (collectableList.Capacity > 0)
+        {
+            collectableList[currentCollectableNum].Use();
+        }
+
+    }
+
+    public void IncrementCurrentNum()
+    {
+        if (currentCollectableNum < collectableList.Count + 1)
+        {
+            currentCollectableNum++;
+        }
+        else
+        {
+            currentCollectableNum = 0;
+        }
+    }
+}
---
+++ CharacterCoding/Assets/Scripts/WeaponCollectable.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class WeaponCollectable : Collectable
+{
+    public UnityEvent useEvent;
+
+    public override void Use()
+    {
+        useEvent.Invoke();
+    }
+
+    public void Attack()
+    {
+        Debug.Log("attack" + this);
+    }
+
+    public void Equip()
+    {
+
+    }
+
+    public void Upgrade()
+    {
+
+    }
+}
